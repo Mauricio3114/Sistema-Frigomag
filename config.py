0bcfd7222da74b554517a9dfad01319a8092f95c
mauricio3114 <engmauriciodomonte@gmail.com>
2026-03-30 22:41:41 -0300

    Corrige banco para Render

--- config.py
+++ config.py
@@ -2,7 +2,4 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-class Config:
-    SECRET_KEY = "frigo-secret-key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "instance", "frigomag.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "frigomag.db")
