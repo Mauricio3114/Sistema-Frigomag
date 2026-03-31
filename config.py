8dd27b77065c31a63f7fdec889083ba9f9391e1a
mauricio3114 <engmauriciodomonte@gmail.com>
2026-03-30 22:49:57 -0300

    Corrige classe Config

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "frigomag.db")
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "frigomag-chave-local")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "frigomag.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
